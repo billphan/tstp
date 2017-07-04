07cf33b8a569abb4cdfbba7cabd1225881f8e95c
Bill Phan <billphan25@gmail.com>
2017-07-04 13:28:15 -0700

    Added user comments and notes.

--- reverse-stack.py
+++ reverse-stack.py
@@ -1,3 +1,5 @@
+# A common programming interview problem - reversing a string using a stack by first putting it in on a stack, then taking it off.
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -19,11 +21,12 @@ class Stack:
         return len(self.items)
 
 stack = Stack()
+# Goes through each character in text and pushes it to the stack.
 for text in "Hello, World!":
     stack.push(text)
 
 reverse = ""
-
+# Iterate through the stack taking letters from text and putting them in reverse variable in reverse order.
 for i in range(len(stack.items)):
     reverse += stack.pop()
 
